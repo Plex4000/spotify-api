2d4e3712607750f7f13842a7be3d3bc281519252
Rich Swartzwelder <rich680@gmail.com>
2018-08-11 11:47:35 -0400

    click on album shows tracks

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Axios from 'axios';
 import './App.css';
 
 const GLOBAL = {
-  key: ""
+  key: "BQAwIge-uSidkrv5FOEkxD9NfwtRSHuHxUH1jKLesPrhPIx0QWCl5fAxvftxQNiqVJmndNZZNjrqz5CZQ39nL02EVhHWR_xHghXD8JYvefigZPYqqBhcxoWiRg5IRdFXxR9zLQYv_w"
 }
 
 class App extends Component {
@@ -15,6 +15,7 @@ class App extends Component {
     this.state = {
       artistInfo: {},
       artistAlbums: [],
+      albumTracks: [],
     }
 
     this.handleGetAlbums = this.handleGetAlbums.bind(this)
@@ -27,7 +28,7 @@ class App extends Component {
 
     // spotify login page
     // step one for token, go here
-    // http://accounts.spotify.com/authorize/?client_id=3588a96280454bf48eb6b0d4dd968d15&response_type=code&redirect_uri=https://localhost:3000&scpe=user-read-private
+    // http://accounts.spotify.com/authorize/?client_id=3588a96280454bf48eb6b0d4dd968d15&response_type=code&redirect_uri=http://localhost:3000&scpe=user-read-private
     // step two for token, get the code returned from this request (it is sitting in the location bar, after localhost:3000)
     // take that code, and put it into postman, in the form/tab called "Spotify Token Get"
     // click "post" and then the new token will come back
@@ -35,7 +36,7 @@ class App extends Component {
 
     Axios.get('https://api.spotify.com/v1/artists/3inCNiUr4R6XQ3W43s9Aqi', {
       headers: {
-        'Authorization': 'Bearer ' + "BQCTNJFCizNHUzo3DQZkXNzlS1jtVMTECn8NIqpJtlznZsidCh8V6hWE73AMfCOHAmyxxfQ49aPTPbkyNbnNLUSBUUA5sQErafx8lHtvCSwIZSEDfiLm0KpmVXJSQtH7iqnc2ZGLiQ"
+        'Authorization': 'Bearer ' + GLOBAL.key
       }
     }).then((response) => {
       this.setState ({
@@ -44,12 +45,11 @@ class App extends Component {
     })
   }
 
-  handleGetAlbums(e) {
-    e.preventDefault()
+  handleGetAlbums() {
 
     Axios.get('https://api.spotify.com/v1/artists/3inCNiUr4R6XQ3W43s9Aqi/albums', {
       headers: {
-        'Authorization': 'Bearer ' + "BQCTNJFCizNHUzo3DQZkXNzlS1jtVMTECn8NIqpJtlznZsidCh8V6hWE73AMfCOHAmyxxfQ49aPTPbkyNbnNLUSBUUA5sQErafx8lHtvCSwIZSEDfiLm0KpmVXJSQtH7iqnc2ZGLiQ"
+        'Authorization': 'Bearer ' + GLOBAL.key
       }
     }).then((response) => {
       this.setState ({
@@ -57,30 +57,64 @@ class App extends Component {
       })
       console.log(this.state.artistAlbums)
     })
-
   }
 
+  handleGetTracks(id) {
+    Axios.get(`https://api.spotify.com/v1/albums/${id}/tracks`, {
+      headers: {
+        'Authorization': 'Bearer ' + GLOBAL.key
+      }
+  }).then((response) => {
+    this.setState ({
+      albumTracks: response.data.items
+    })
+  })
+}
+
+getAlbums() {
+  if (this.state.artistAlbums) {
+    return (
+    <div>{
+      this.state.artistAlbums.map(album => {
+        return  <div key={album.id}>
+                  <span onClick={this.handleGetTracks.bind(this, album.id)}>{album.name}</span>
+                </div>
+        })
+      }
+      </div>
+    )
+  }
+  return null;
+}
+
+ getTracks() {
+  if (this.state.albumTracks) {
+    return (
+    <div>{
+      this.state.albumTracks.map(track => {
+        return  <div key={track.id}>
+                  <span>{track.name}</span>
+                </div>
+        })
+      }
+      </div>
+    )
+  }
+  return null;
+ }
   render() {
 
-    let albums = null
+    let albums = this.getAlbums();
 
-        if (this.state.artistAlbums) {
-          albums = (
-          <div className="movieContainer">{
-            this.state.artistAlbums.map(album => {
-              return  <div key={album.id}>
-                        <span onClick={this.handleGetTracks}>{album.name}</span>
-                      </div>
-              })
-            }
-           </div>
-          )
-        }
+    let tracks = this.getTracks();
+    
     return (
       <div>
         <h1>{this.state.artistInfo.name}</h1>
         <button onClick={this.handleGetAlbums}>View albums</button>
         <div>{albums}</div>
+        <hr />
+        <div>{tracks}</div>
       </div>
     );
   }
